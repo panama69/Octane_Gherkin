38eef086ab6284c4f59e298e3e473fc6ed01cfe3
Dave Flynn <david.flynn@hpe.com>
2017-07-19 10:33:04 -0500

    Update TestRunner.java

--- src/test/java/net/hpe/TestRunner.java
+++ src/test/java/net/hpe/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(OctaneCucumber.class)
-@CucumberOptions(plugin = {"junit:junitResult.xml"},
+@CucumberOptions(plugin = {"junit:junitResults.xml"},
                  features = "src/test/resources/features")
 public class TestRunner {
 
